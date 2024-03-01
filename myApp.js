3370464f1fd077829c55cd858cc3a813fa9bd553
M2 <italiyankin@gmail.com>
2024-03-01 13:26:03 +0000

    fix syntaxt

--- myApp.js
+++ myApp.js
@@ -52,7 +52,7 @@ let personName = "a2"
 
 const findPeopleByName = (personName, done) => {
   
-  Person.findById(personName, function (err, people) {
+  Person.findById({name:personName}, function (err, people) {
     if (err) return console.error(err);
     done(null, people);
   })
